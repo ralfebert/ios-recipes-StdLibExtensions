e9ecf380e3b98040eecc35f185a7b756d1fac1e8
Ralf Ebert <info@ralfebert.de>
2018-01-31 21:31:25 +0100

    shuffle method

--- StdLibExtensions/Helper/Array+Sample.swift
+++ StdLibExtensions/Helper/Array+Sample.swift
@@ -6,12 +6,20 @@ import Darwin
 
 extension Array {
     
+    /**
+     * Returns a random element of the Array or nil if the Array is empty.
+     */
     var sample : Element? {
         guard !isEmpty else { return nil }
         return self[Int(arc4random_uniform(UInt32(self.count)))]
     }
-
-    func sample(count : Int) -> [Element] {
+    
+    /**
+     * Returns `count` random elements from the array.
+     * If there are not enough elements in the Array, a smaller Array is returned.
+     * Elements will not be returned twice except when there are duplicate elements in the original Array.
+     */
+    func sample(_ count : Int) -> [Element] {
         let sampleCount = Swift.min(count, self.count)
         
         var elements = self
@@ -25,4 +33,26 @@ extension Array {
         return samples
     }
 
+    /**
+     * Shuffles the elements in the Array in-place using the
+     * [Fisher-Yates shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle).
+     */
+    mutating func shuffle() {
+        guard self.count >= 1 else { return }
+
+        for i in (1..<self.count).reversed() {
+            let j = Int(arc4random_uniform(UInt32(i + 1)))
+            self.swapAt(j, i)
+        }
+    }
+    
+    /**
+      * Returns a new Array with the elements in random order.
+      */
+    var shuffled : [Element] {
+        var elements = self
+        elements.shuffle()
+        return elements
+    }
+    
 }
--- StdLibExtensionsTests/Array+SampleTest.swift
+++ StdLibExtensionsTests/Array+SampleTest.swift
@@ -6,20 +6,27 @@ class ArraySampleTest: XCTestCase {
     func testEmptyArray() {
         let emptyArray = [Int]()
         XCTAssertNil(emptyArray.sample)
-        XCTAssertEqual([], emptyArray.sample(count: 1))
+        XCTAssertEqual([], emptyArray.sample(1))
+        XCTAssertEqual([], emptyArray.shuffled)
     }
     
     func testSingleElement() {
         XCTAssertEqual(1, [1].sample)
-        XCTAssertEqual([1], [1].sample(count: 1))
-        XCTAssertEqual([1], [1].sample(count: 2))
+        XCTAssertEqual([1], [1].sample(1))
+        XCTAssertEqual([1], [1].sample(2))
+        XCTAssertEqual([1], [1].shuffled)
     }
 
     func testMultipleElements() {
-        let numbers = Array(1...10)
-        let sample = numbers.sample(count: 3)
-        XCTAssertEqual(3, Set(sample).count)
+        let numbers = Array(1...49)
+        let sample = numbers.sample(6)
+        XCTAssertEqual(6, Set(sample).count)
         XCTAssertEqual(Set(sample), Set(numbers).intersection(sample))
+        let shuffled = numbers.shuffled
+        XCTAssertEqual(numbers.count, Set(shuffled).count)
+        XCTAssertNotEqual(numbers, shuffled)
+        print("sample:", sample)
+        print("shuffled:", shuffled)
     }
 
 }
